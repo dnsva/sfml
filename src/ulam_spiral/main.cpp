57a816051c2972b1ea789d26d49ce433460c987f
Anna <peachtiger0@gmail.com>
2024-07-19 11:12:59 -0400

    cleaned up code

--- src/ulam_spiral/main.cpp
+++ src/ulam_spiral/main.cpp
@@ -9,10 +9,9 @@
 
 int total_rows = 200, total_cols = 200; //set default values
 
-bool isPrime(int n) {
-    if (n <= 1) return false;
+bool is_prime(int n){ //check if n is prime
+    if (n <= 1 || n % 2 == 0 || n % 3 == 0) return false;
     if (n <= 3) return true;
-    if (n % 2 == 0 || n % 3 == 0) return false;
     for (int i = 5; i * i <= n; i += 6) {
         if (n % i == 0 || n % (i + 2) == 0) return false;
     }
@@ -21,9 +20,8 @@ bool isPrime(int n) {
 
 sf::Color getColor(int i, int j){ //calculate RGB values based on i and j
     sf::Uint8 red = static_cast<sf::Uint8>((static_cast<float>(j) * 128) / total_cols);
-    sf::Uint8 green =static_cast<sf::Uint8>(128);
     sf::Uint8 blue = static_cast<sf::Uint8>((static_cast<float>(i) * 255) / total_rows);
-    return sf::Color(red, green, blue);
+    return sf::Color(red, static_cast<sf::Uint8>(128), blue); //green is constant
 }
 
 void drawSquare(int i, int j, sf::RenderWindow &window, sf::Vector2f cell_size, bool prime){
@@ -35,9 +33,14 @@ void drawSquare(int i, int j, sf::RenderWindow &window, sf::Vector2f cell_size, 
     window.draw(cell);
 }
 
-int main() {
+int main(int argc, char* argv[]){
 
-    sf::RenderWindow window(sf::VideoMode(500,500), "Ulam's Spiral");
+    if(argc == 2 && std::stoi(argv[1]) > 0){ //check for valid cmd line args
+        total_rows = std::stoi(argv[1]);
+        total_cols = std::stoi(argv[1]);
+    } //else - default values are used
+
+    sf::RenderWindow window(sf::VideoMode(500,500), "Ulam's Spiral"); //create window
 
     while (window.isOpen()) { //continues until window is closed
         sf::Event event; //handle events
@@ -46,35 +49,32 @@ int main() {
         }
         window.clear(); //clear the window
 
-       //set size of cells basd on window size
-        sf::Vector2f window_size = window.getView().getSize();
+        sf::Vector2f window_size = window.getView().getSize(); //set size of cells based on window size
         sf::Vector2f cell_size(window_size.x / total_cols, window_size.y / total_rows);
-
         int r = total_rows/2-1, c = total_cols/2-1; //start at the center
-
-        int counter = 1; //the actual number in the spiral
+        int counter = 1; //the actual CURRENT number in the spiral
 
         for(int i = 1; i < ceil((total_rows - 1)/2.0); ++i){
             int s = (i*8-4)/4; //side length
             for(int j = 0; j < s; ++j){
-                drawSquare(r-j, c+1, window, cell_size, isPrime(++counter));
+                drawSquare(r-j, c+1, window, cell_size, is_prime(++counter));
             }
-            drawSquare(r-s, c+1, window, cell_size, isPrime(++counter)); //top right pivot
+            drawSquare(r-s, c+1, window, cell_size, is_prime(++counter)); //top right pivot
             r = r-s+1; //update r, c
             for(int j = 0; j < s; ++j){
-                drawSquare(r-1, c-j, window, cell_size, isPrime(++counter));
+                drawSquare(r-1, c-j, window, cell_size, is_prime(++counter));
             }
-            drawSquare(r-1, c-s, window, cell_size, isPrime(++counter)); //top left pivot
+            drawSquare(r-1, c-s, window, cell_size, is_prime(++counter)); //top left pivot
             c = c-s+1; //update r, c
             for(int j = 0; j < s; ++j){
-                drawSquare(r+j, c-1, window, cell_size, isPrime(++counter));
+                drawSquare(r+j, c-1, window, cell_size, is_prime(++counter));
             }
-            drawSquare(r+s, c-1, window, cell_size, isPrime(++counter)); //bottom left pivot
+            drawSquare(r+s, c-1, window, cell_size, is_prime(++counter)); //bottom left pivot
             r = r+s-1; //update r, c
             for(int j = 0; j < s; ++j){
-                drawSquare(r+1, c+j, window, cell_size, isPrime(++counter));
+                drawSquare(r+1, c+j, window, cell_size, is_prime(++counter));
             }
-            drawSquare(r+1, c+s, window, cell_size, isPrime(++counter)); //bottom right pivot
+            drawSquare(r+1, c+s, window, cell_size, is_prime(++counter)); //bottom right pivot
             r = r+1; //update r to the pivot
             c = c+s; //update c to the pivot
         }

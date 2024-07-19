ec23eceef8f2ef937e475656aab4febb3fa7d384
Anna <peachtiger0@gmail.com>
2024-07-19 19:47:52 -0400

    need to work out math

--- src/sierpinski_triangle/main.cpp
+++ src/sierpinski_triangle/main.cpp
@@ -9,6 +9,55 @@
 //Function
 //Given a triangle at position (x, y) with size s, the function draw three triangles in the corners colored white and one in the center colored black
 
+void drawTriangle(sf::RenderWindow& window, float x, float y, float s) {
+    
+    //Create the big triangle 
+    sf::CircleShape triangle(s, 3);
+    
+    triangle.setPosition(x, y);
+    window.draw(triangle);
+
+    if(s < 1.0f) { //break condition
+        triangle.setFillColor(sf::Color::Black);
+        return;
+    }
+
+    //We have a triangle at x, y with size s
+    
+    triangle.setFillColor(sf::Color::White);
+
+    //Now we need black triangles in each corner that also call recursive calls
+
+    //Top triangle
+
+    sf::CircleShape triangleTop(s / 2.0f, 3);
+    triangleTop.setPosition(x + s / 2.0f, y);
+    triangleTop.setFillColor(sf::Color::Blue);
+    window.draw(triangleTop);
+
+   // drawTriangle(window, x + s / 2.0f, y, s / 2.0f);
+
+
+    /*Y = sqrt(s^2 - s^2/4) - sqrt(s^2/4 - s^2/16)
+    Y = sqrt(3s^2 / 4) - sqrt( 3s^2 / 16)
+    Y = sqrt(3/4 * s^2) - sqrt 3/16 * s^2)
+    Y = s*sqrt(3/4) - s*sqrt(3/16)
+    Y = s(sqrt(3/4) - sqrt(3/16))
+    Y = s * 0.43301270189   */
+
+    //Bottom left
+
+    sf::CircleShape triangleBottomLeft(s / 2.0f, 3);
+    triangleBottomLeft.setPosition(x, y + sqrt((s)*(s) - (s/2)*(s/2))); //???
+    triangleBottomLeft.setFillColor(sf::Color::Blue);
+    window.draw(triangleBottomLeft);
+
+  //  drawTriangle(window, x, y + s * 0.433f, s / 2.0f);
+    //Bottom right
+ //   drawTriangle(window, x + s, y + s * 0.433f, s / 2.0f);
+    
+}
+
 
 int main() {
   
@@ -27,12 +76,16 @@ int main() {
 
         // Clear the window
         window.clear();
+
+        //Draw the triangle
+        drawTriangle(window, 0, 0, 300);
         
          //TEST
+         /*
         sf::CircleShape triangle(80, 3);
         triangle.setFillColor(sf::Color::White);
         triangle.setPosition(100,800-69.3f);
-        window.draw(triangle);
+        window.draw(triangle); */
 
 //        sf::Vector2f windowSize = window.getView().getSize();
 

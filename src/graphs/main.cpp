f7258650d89dfd5c1f822754013b2bff0ee8ec70
Anna <peachtiger0@gmail.com>
2024-08-04 18:22:14 -0400

    ok

--- src/graphs/main.cpp
+++ src/graphs/main.cpp
@@ -5,9 +5,6 @@
 #include <utility>
 #include <iostream>
 
-#include "../annas_library/button.h"
-#include "../annas_library/textfield.h"
-
 using namespace std;
 
 //Compile:
@@ -33,7 +30,7 @@ struct node{
    
 };
 
-void draw_grid(sf::RenderWindow& window, sf::Font font, int cols, int rows) {
+void draw_grid(sf::RenderWindow& window, sf::Font& font, int cols, int rows) {
     /* THE FOLLOWING CODE DRAWS GRID LINES AND NUMBERS AT THE TOP AND SIDES */
     for(int i = 0; i < 800; i+=50){
         sf::Text col_text;
@@ -70,10 +67,84 @@ void draw_grid(sf::RenderWindow& window, sf::Font font, int cols, int rows) {
     /***************************************************************************/
 }
 
-void setup_nodes(sf::RenderWindow& window, vector<node>& nodes, sf::Font font, int cols = 100, int rows = 100){
+sf::Text create_text(sf::Font& font, string text, int x, int y, int size = 12, sf::Color color = sf::Color::Magenta){
+    sf::Text t;
+    t.setString(text);
+    t.setFont(font);
+    t.setCharacterSize(size);
+    t.setFillColor(color);
+    t.setPosition(x, y);
+    return t;
+}
+
+void setup_nodes(sf::RenderWindow& window, vector<node>& nodes, sf::Font& font, int cols = 100, int rows = 100){
+
+    /* DRAW BOX (BOTTOM LEFT) WITH THE FOLLOWING 
+        - ADD NODE OPTION 
+        - REMOVE NODE 
+        - ADD/REMOVE EDGE
+    */
+
+    /* DRAW A BOX - BOTTOM LEFT */
+        sf::RectangleShape box(sf::Vector2f(300.f, 200.f));
+        box.setFillColor(sf::Color(100,100,100));
+        box.setPosition(0, 600);
+
+    /* ADD NODE OPTION
+        Title (centered): "Add Node"
+        Text: "Name: ", Box: (input), Text: (user input if box clicked)
+        Text: "X-Coord; ", Box: (input), Text: (user input if box clicked)
+        Text: "Y-Coord; ", Box: (input), Text: (user input if box clicked)
+        Button: "OK" (if clicked, add node to graph)
+    */
+    sf::Text add_node_title = create_text(font, "Add Node", 100, 620);
+    sf::Text add_node_name = create_text(font, "Name: ", 10, 650);
+    sf::Text add_node_x = create_text(font, "X-Coord: ", 10, 670);
+    sf::Text add_node_y = create_text(font, "Y-Coord: ", 10, 690);
+
+    //input boxes:
+    sf::RectangleShape add_node_name_box(sf::Vector2f(100.f, 20.f));
+    add_node_name_box.setFillColor(sf::Color::White);
+    add_node_name_box.setPosition(60, 650);
+
+    string add_node_name_input = "juhu";
+    sf::Text add_node_name_input_text = create_text(font, add_node_name_input, 60, 650);
+
+    sf::RectangleShape x_box(sf::Vector2f(100.f, 20.f));
+    x_box.setFillColor(sf::Color::White);
+    x_box.setPosition(60, 670);
+
+    string x_input = "ljlkjlkj";
+    sf::Text x_input_text = create_text(font, x_input, 60, 670);
+
+    sf::RectangleShape y_box(sf::Vector2f(100.f, 20.f));
+    y_box.setFillColor(sf::Color::White);
+    y_box.setPosition(60, 690);
+
+    string y_input = "kkk";
+    sf::Text y_input_text = create_text(font, y_input, 60, 690);
+
+    //ok button:
+    sf::RectangleShape add_node_ok_button(sf::Vector2f(50.f, 20.f));
+    add_node_ok_button.setFillColor(sf::Color::White);
+    add_node_ok_button.setPosition(60, 710);
+
+    sf::Text add_node_ok_text = create_text(font, "OK", 50.f, 20.f)
 
 
-    //sdfsdfsdf
+
+    /* REMOVE NODE OPTION
+        Title (centered): "Remove Node"
+        Text: "Name: ", Box: (input), Text: (user input if box clicked)
+        Button: "OK" (if clicked, remove node from graph)
+    */
+
+    /* ADD/REMOVE EDGE OPTION
+        Title (centered): "Add/Remove Edge"
+        Text: "Node 1: ", Box: (input), Text: (user input if box clicked)
+        Text: "Node 2: ", Box: (input), Text: (user input if box clicked)
+        Button: "OK" (if clicked, add edge to graph if already doesnt exist, if it does exist, remove the edge
+    */
 
 
     while(window.isOpen()){
@@ -88,75 +159,34 @@ void setup_nodes(sf::RenderWindow& window, vector<node>& nodes, sf::Font font, i
 
         window.clear();
 
-        draw_grid(window, font, cols, rows);
+       // draw_grid(window, font, cols, rows);
 
-        for(auto n : nodes){
-            window.draw(n.circle);
-            window.draw(n.circle_name);
-        }
+        window.draw(box);
+        window.draw(add_node_title);
+        window.draw(add_node_name);
+        window.draw(add_node_x);
+        window.draw(add_node_y);
+        window.draw(add_node_name_box);
+        window.draw(add_node_name_input_text);
+        window.draw(x_box);
+        window.draw(x_input_text);
+        window.draw(y_box);
+        window.draw(y_input_text);
+        window.draw(add_node_ok_button);
+
+
+
+
+     //   for(auto n : nodes){
+       //     window.draw(n.circle);
+         //   window.draw(n.circle_name);
+        //}
 
         window.display();
 
     }
 
-    /* DRAW BOX (BOTTOM LEFT) WITH THE FOLLOWING 
-        - ADD NODE OPTION 
-        - REMOVE NODE 
-        - ADD/REMOVE EDGE
-        */
-
-       /* DRAW A BOX - BOTTOM LEFT */
-        sf::RectangleShape box(sf::Vector2f(300.f, 200.f));
-        box.setFillColor(sf::Color(100,100,100));
-        box.setPosition(0, 600);
-        
-
-        /* ADD NODE OPTION
-        row: [textfield]
-        col: [textfield]
-        [button: "OK"]
-
-        button action() - 
-            if rows and cols strings ARENT empty and are both NUMBERS
-            create new node at (row, col)
-            nodes.push_back(node)
-        */
-
-      //  textfield row_textfield(0, 600, 100, 50);
-       //textfield col_textfield(100, 600, 100, 50);
-       // button ok_button("OK", 200, 600, 100, 50);
-        
-/*
-        ok_button.button_action = [&](){
-            if(!row_textfield.curr_string.empty() && !col_textfield.curr_string.empty()){
-                bool is_number = true;
-                for(auto c : row_textfield.curr_string){
-                    if(!isdigit(c)){
-                        is_number = false;
-                        break;
-                    }
-                }
-                for(auto c : col_textfield.curr_string){
-                    if(!isdigit(c)){
-                        is_number = false;
-                        break;
-                    }
-                }
-                if(is_number){
-                    node n;
-                    n.x = stoi(col_textfield.curr_string);
-                    n.y = stoi(row_textfield.curr_string);
-                    n.circle.setPosition(n.x, n.y);
-                    n.circle_name.setFont(font);
-                    n.circle_name.setString("A"); //for now. also add textfield later for name 
-                    n.circle_name.setCharacterSize(24);
-                    n.circle_name.setFillColor(sf::Color::White);
-                    n.circle_name.setPosition(n.x, n.y);
-                    nodes.push_back(n);
-                }
-            }
-        };
-*/
+    
     
 }
 
@@ -186,8 +216,8 @@ int main(){
     font.loadFromFile("src/fonts/arial.ttf");
 
     setup_nodes(window, nodes, font);
-    choose_algorithm();
-    run_algorithm();
+  //  choose_algorithm();
+    //run_algorithm();
 
     return 0;
 }
